e0a271d1dcdeb625933450498975bd64418c6cb6
Mateus <s.mateussales@gmail.com>
2021-08-30 02:29:51 -0300

    Implementação do Dialog ao remover contato

--- app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agenda/ui/activity/ListaAlunosActivity.java
@@ -2,6 +2,8 @@ package br.com.alura.agenda.ui.activity;
 
 import static br.com.alura.agenda.ui.activity.ConstantesActivities.CHAVE_ALUNO;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -36,7 +38,6 @@ public class ListaAlunosActivity extends AppCompatActivity {
         configuraFabNovoAluno();
         configuraLista();
 
-
     }
 
     @Override
@@ -50,14 +51,24 @@ public class ListaAlunosActivity extends AppCompatActivity {
         int itemId = item.getItemId();
         CharSequence tituloDoMenu = item.getTitle();
         if (itemId == R.id.activity_lista_alunos_menu_remover){
-            AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-            Aluno alunoEscolhido = adapter.getItem(menuInfo.position);
-            remove(alunoEscolhido);
+            confirmaRemocao(item);
 
         }
 
         return super.onContextItemSelected(item);
 
+    }
+
+    private void confirmaRemocao(final MenuItem item) {
+        new AlertDialog.Builder(this).setTitle("Removendo Contato").setMessage("Tem certeza que deseja remover o contato?")
+                .setPositiveButton("sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                        Aluno alunoEscolhido = adapter.getItem(menuInfo.position);
+                        remove(alunoEscolhido);
+                    }
+                }).setNegativeButton("Não", null).show();
     }
 
     private void configuraFabNovoAluno() {
